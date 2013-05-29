594065fb58b5ecbbedfd0a899764d1359b51cf97
James Bergstra <james.bergstra@gmail.com>
2013-05-29 11:26:21 -0400

    Partial init of reference from nengo_ocl
    A basic test of making an ensemble using the old_api.py works,
    but probes are not implemented, so I can't even verify the output yet.
    Things are at a very early stage.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    try:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup
+    except Exception, e:
+        print "Forget setuptools, trying distutils..."
+        from distutils.core import setup
+
+
+setup(
+    name="sigops",
+    version="0.1.0",
+    author="",
+    author_email="",
+    packages=['nengo', 'nengo.tests'],
+    scripts=[],
+    url="",
+    license="BSD",
+    description="",
+    long_description="",
+    requires=[
+        "numpy (>=1.5.0)",
+        "networkx",
+    ],
+)
