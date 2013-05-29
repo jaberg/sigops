e0cd7362f3e30404ac57710595fb868af2a77f40
James Bergstra <james.bergstra@gmail.com>
2013-05-29 14:08:49 -0400

    Cleaned up base.py
    * removed old nengo_theano wrapper stuff
    * added docs
    * added less-global random-state thing for generating weights (should del)
    * added rate-mode populations

--- nengo/base.py
+++ nengo/base.py
@@ -1,24 +1,37 @@
+"""
+base.py: model description classes
+
+These classes are used to describe a nengo model (Model).
+Model is the input to a *simulator* (see e.g. simulator.py).
+
+"""
 import numpy as np
+
+random_weight_rng = np.random.RandomState(12345)
 
 
 class Signal(object):
+    """Interpretable, vector-valued quantity within NEF"""
     def __init__(self, n=1):
         self.n = n
 
 
 class SignalProbe(object):
+    """A model probe to record a signal"""
     def __init__(self, sig, dt):
         self.sig = sig
         self.dt = dt
 
 
 class Constant(Signal):
+    """A signal meant to hold a fixed value"""
     def __init__(self, n, value):
         Signal.__init__(self, n)
         self.value = value
 
 
 class Transform(object):
+    """A linear transform from a decoded signal to the signals buffer"""
     def __init__(self, alpha, insig, outsig):
         self.alpha = alpha
         self.insig = insig
@@ -26,6 +39,7 @@ class Transform(object):
 
 
 class CustomTransform(object):
+    """An arbitrary transform from a decoded signal to the signals buffer"""
     def __init__(self, func, insig, outsig):
         self.func = func
         self.insig = insig
@@ -33,6 +47,7 @@ class CustomTransform(object):
 
 
 class Filter(object):
+    """A linear transform from signals[t-1] to signals[t]"""
     def __init__(self, alpha, oldsig, newsig):
         self.oldsig = oldsig
         self.newsig = newsig
@@ -40,6 +55,9 @@ class Filter(object):
 
 
 class Model(object):
+    """
+    A container for model components.
+    """
     def __init__(self, dt):
         self.dt = dt
         self.signals = []
@@ -49,6 +67,7 @@ class Model(object):
         self.signal_probes = []
 
     def signal(self, n=1, value=None):
+        """Add a signal to the model"""
         if value is None:
             rval = Signal(n)
         else:
@@ -57,21 +76,25 @@ class Model(object):
         return rval
 
     def signal_probe(self, sig, dt):
+        """Add a signal probe to the model"""
         rval = SignalProbe(sig, dt)
         self.signal_probes.append(rval)
         return rval
 
     def transform(self, alpha, insig, outsig):
+        """Add a transform to the model"""
         rval = Transform(alpha, insig, outsig)
         self.transforms.append(rval)
         return rval
 
     def filter(self, alpha, oldsig, newsig):
+        """Add a filter to the model"""
         rval = Filter(alpha, oldsig, newsig)
         self.filters.append(rval)
         return rval
 
     def custom_transform(self, func, insig, outsig):
+        """Add a custom transform to the model"""
         rval = CustomTransform(func, insig, outsig)
         self.custom_transforms.append(rval)
         return rval
