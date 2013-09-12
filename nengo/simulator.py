fa565de4c7c4b4f2d63d33b90c47d2bf642e6ded
drasmuss <drasmuss@uwaterloo.ca>
2013-09-12 11:34:06 -0400

    More conditions checked for digraph
    - Changed ProdUpdate to np.dot

--- nengo/simulator.py
+++ nengo/simulator.py
@@ -281,12 +281,10 @@ class ProdUpdate(Operator):
                     val = np.asarray(val).reshape(Y.shape)
                 else:
                     raise ValueError('shape mismatch in %s (%s vs %s)' %
-                                     (self.tag, val.shape, Y.shape))
+                                     (self.tag, val, Y))
 
-            Y[...] *= B
+            Y[...] = np.dot(B,Y)
             Y[...] += val
-
-
 
         return step
 
@@ -351,15 +349,31 @@ class Simulator(object):
         for node in sets:
             assert len(sets[node]) == 1, (node, sets[node])
 
+        # -- assert that only one op updates any particular view
+        for node in ups:
+            assert len(ups[node]) == 1, (node, ups[node])
+
+        # --- assert that any node that is incremented is also set/updated
+        for node in incs:
+            assert len(sets[node]+ups[node]) > 0, (node)
+
         # -- assert that no two views are both set and aliased
         if len(sets) >= 2:
             for node, other in itertools.combinations(sets, 2):
-                assert not node.shares_memory_with(other)
+                assert not node.shares_memory_with(other), \
+                    ("%s shares memory with %s" % (node, other))
+
+        # -- assert that no two views are both updated and aliased
+        if len(ups) >= 2:
+            for node, other in itertools.combinations(ups, 2):
+                assert not node.shares_memory_with(other), (
+                        node, other)
 
         # -- Scheduling algorithm for serial evaluation:
         #    1) All sets on a given base signal
         #    2) All incs on a given base signal
         #    3) All reads on a given base signal
+        #    4) All updates on a given base signal
 
         # -- incs depend on sets
         for node, post_ops in incs.items():
@@ -374,16 +388,6 @@ class Simulator(object):
             for other in by_base_writes[node.base]:
                 pre_ops += sets[other] + incs[other]
             dg.add_edges_from(itertools.product(set(pre_ops), post_ops))
-
-        # -- assert that only one op updates any particular view
-        for node in ups:
-            assert len(ups[node]) == 1, (node, ups[node])
-
-        # -- assert that no two views are both updated and aliased
-        if len(ups) >= 2:
-            for node, other in itertools.combinations(ups, 2):
-                assert not node.shares_memory_with(other), (
-                        node, other)
 
         # -- updates depend on reads, sets, and incs.
         for node, post_ops in ups.items():
