80a39400bdbacac6f66d55d3fcacedfdb0c5fb54
Trevor Bekolay <tbekolay@gmail.com>
2013-10-23 13:06:59 -0400

    Removed is_base and is_view (never used)
    Also changed imports in test_simulator a bit.

--- nengo/tests/test_simulator.py
+++ nengo/tests/test_simulator.py
@@ -6,24 +6,26 @@ except ImportError:
 import numpy as np
 
 import nengo.simulator as simulator
-import nengo.base as base
+from nengo.base import Signal, Constant
 
 
 class TestSimulator(unittest.TestCase):
     def test_signal_indexing_1(self):
-        one = base.Signal(n=1, name='a')
-        two = base.Signal(n=2, name='b')
-        three = base.Signal(n=3, name='c')
+        one = Signal(n=1, name='a')
+        two = Signal(n=2, name='b')
+        three = Signal(n=3, name='c')
 
-        tmp = base.Signal(n=3, name='tmp')
+        tmp = Signal(n=3, name='tmp')
 
         operators = []
-        operators += [simulator.ProdUpdate(base.Constant(1), three[0:1], base.Constant(0), one)]
-        operators += [simulator.ProdUpdate(base.Constant(2.0), three[1:], base.Constant(0), two)]
+        operators += [simulator.ProdUpdate(
+            Constant(1), three[0:1], Constant(0), one)]
+        operators += [simulator.ProdUpdate(
+            Constant(2.0), three[1:], Constant(0), two)]
         operators += [
             simulator.Reset(tmp),
             simulator.DotInc(
-                base.Constant([[0,0,1],[0,1,0],[1,0,0]]),
+                Constant([[0,0,1],[0,1,0],[1,0,0]]),
                 three,
                 tmp),
             simulator.Copy(src=tmp, dst=three, as_update=True),
