1936fc21e4d14f6675e146f320e512d939de0fbc
Trevor Bekolay <tbekolay@gmail.com>
2013-10-24 14:01:49 -0400

    Moved all build methods to builder.py
    - Create Builder class (with some decorators to keep things clean)
    - Build is now completely isolated from creation and simulation
    - A builder can be also be a context manager;
      see tests/test_simulator.py for an example
    - Interesting side effect: there is a timestep 0 now,
      which matches the Java version

--- nengo/tests/test_simulator.py
+++ nengo/tests/test_simulator.py
@@ -7,9 +7,24 @@ import numpy as np
 
 import nengo
 import nengo.simulator as simulator
+from nengo.builder import Builder
 from nengo.builder import Signal, Constant
 from nengo.builder import DotInc, ProdUpdate, Reset, Copy
 
+
+class TestBuilder(object):
+    def __enter__(self):
+        simulator.Simulator.builder = self
+
+    def __exit__(self, type, value, traceback):
+        simulator.Simulator.builder = Builder()
+
+    def __call__(self, model, dt):
+        model.dt = dt
+        model.seed = 0
+        if not hasattr(model, 'probes'):
+            model.probes = []
+        return model
 
 class TestSimulator(unittest.TestCase):
     def test_steps(self):
@@ -32,36 +47,32 @@ class TestSimulator(unittest.TestCase):
 
     def test_signal_indexing_1(self):
         m = nengo.Model("test_signal_indexing_1")
-        one = m.add(Signal(n=1, name='a'))
-        two = m.add(Signal(n=2, name='b'))
-        three = m.add(Signal(n=3, name='c'))
 
-        tmp = m.add(Signal(n=3, name='tmp'))
+        one = Signal(n=1, name='a')
+        two = Signal(n=2, name='b')
+        three = Signal(n=3, name='c')
+        tmp = Signal(n=3, name='tmp')
+        m.signals = [one, two, three, tmp]
 
-        m._operators += [ProdUpdate(
-            Constant(1), three[0:1], Constant(0), one)]
-        m._operators += [ProdUpdate(
-            Constant(2.0), three[1:], Constant(0), two)]
-        m._operators += [
+        m.operators = [
+            ProdUpdate(Constant(1), three[:1], Constant(0), one),
+            ProdUpdate(Constant(2.0), three[1:], Constant(0), two),
             Reset(tmp),
-            DotInc(
-                Constant([[0,0,1],[0,1,0],[1,0,0]]),
-                three,
-                tmp),
+            DotInc(Constant([[0,0,1],[0,1,0],[1,0,0]]), three, tmp),
             Copy(src=tmp, dst=three, as_update=True),
-            ]
+        ]
 
-        sim = m.simulator(sim_class=simulator.Simulator)
-        memo = sim.model.memo
-        sim.signals[sim.get(three)] = np.asarray([1, 2, 3])
-        sim.step()
-        self.assertTrue(np.all(sim.signals[sim.get(one)] == 1))
-        self.assertTrue(np.all(sim.signals[sim.get(two)] == [4, 6]))
-        self.assertTrue(np.all(sim.signals[sim.get(three)] == [3, 2, 1]))
-        sim.step()
-        self.assertTrue(np.all(sim.signals[sim.get(one)] == 3))
-        self.assertTrue(np.all(sim.signals[sim.get(two)] == [4, 2]))
-        self.assertTrue(np.all(sim.signals[sim.get(three)] == [1, 2, 3]))
+        with TestBuilder():
+            sim = m.simulator(sim_class=simulator.Simulator)
+            sim.signals[three] = np.asarray([1, 2, 3])
+            sim.step()
+            self.assertTrue(np.all(sim.signals[one] == 1))
+            self.assertTrue(np.all(sim.signals[two] == [4, 6]))
+            self.assertTrue(np.all(sim.signals[three] == [3, 2, 1]))
+            sim.step()
+            self.assertTrue(np.all(sim.signals[one] == 3))
+            self.assertTrue(np.all(sim.signals[two] == [4, 2]))
+            self.assertTrue(np.all(sim.signals[three] == [1, 2, 3]))
 
 
 if __name__ == "__main__":
