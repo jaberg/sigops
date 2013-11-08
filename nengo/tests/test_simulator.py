ce5e196ed021f38feb2e490b3cf52242cecf096e
Trevor Bekolay <tbekolay@gmail.com>
2013-11-08 18:29:03 -0500

    Simplified Signal constructor
    Now, a value must be specified when creating a Signal.
    
    Also sped up DotInc and ProdUpdate's step function by caching
    the check for whether they need to reshape (see reshape_dot).

--- nengo/tests/test_simulator.py
+++ nengo/tests/test_simulator.py
@@ -13,37 +13,38 @@ from nengo.base import DotInc, ProdUpdate, Reset, Copy
 class TestSimulator(unittest.TestCase):
     def test_signal_init_values(self):
         """Tests that initial values are not overwritten."""
-        zero = Signal(value=[0])
-        one = Signal(value=[1])
-        five = Signal(value=[5.0])
-        zeroarray = Signal(value=np.array([[0,0,0]]))
-        array = Signal(value=np.array([1,2,3]))
-        signals = [zero, one, five, array]
+        zero = Signal([0])
+        one = Signal([1])
+        five = Signal([5.0])
+        zeroarray = Signal([[0,0,0]])
+        array = Signal([1,2,3])
         operators = [ProdUpdate(zero, zero, one, five),
                      ProdUpdate(one, zeroarray, one, array)]
 
         sim = simulator.Simulator(operators)
-        self.assertEqual(1, sim.signals[one])
-        self.assertEqual(5.0, sim.signals[five])
+        self.assertEqual(0, sim.signals[zero][0])
+        self.assertEqual(1, sim.signals[one][0])
+        self.assertEqual(5.0, sim.signals[five][0])
         self.assertTrue(np.all(
             np.array([1,2,3]) == sim.signals[array]))
         sim.step()
-        self.assertEqual(1, sim.signals[one])
-        self.assertEqual(5.0, sim.signals[five])
+        self.assertEqual(0, sim.signals[zero][0])
+        self.assertEqual(1, sim.signals[one][0])
+        self.assertEqual(5.0, sim.signals[five][0])
         self.assertTrue(np.all(
             np.array([1,2,3]) == sim.signals[array]))
 
     def test_signal_indexing_1(self):
-        one = Signal(shape=1, name='a')
-        two = Signal(shape=2, name='b')
-        three = Signal(shape=3, name='c')
-        tmp = Signal(shape=3, name='tmp')
+        one = Signal(np.zeros(1), name='a')
+        two = Signal(np.zeros(2), name='b')
+        three = Signal(np.zeros(3), name='c')
+        tmp = Signal(np.zeros(3), name='tmp')
 
         operators = [
-            ProdUpdate(Signal(value=1), three[0:1], Signal(value=0), one),
-            ProdUpdate(Signal(value=2.0), three[1:], Signal(value=0), two),
+            ProdUpdate(Signal(1), three[:1], Signal(0), one),
+            ProdUpdate(Signal(2.0), three[1:], Signal(0), two),
             Reset(tmp),
-            DotInc(Signal(value=[[0,0,1],[0,1,0],[1,0,0]]), three, tmp),
+            DotInc(Signal([[0,0,1],[0,1,0],[1,0,0]]), three, tmp),
             Copy(src=tmp, dst=three, as_update=True),
         ]
 
