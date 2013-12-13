1f461a74f0da39e1882c2d5b5c9037d2d7240018
James Bergstra <james.bergstra@gmail.com>
2013-12-13 13:08:07 -0500

    Make Operator signal roles explicit

--- nengo/builder.py
+++ nengo/builder.py
@@ -364,26 +364,61 @@ class Operator(object):
     #     memory.
     #
 
-    # -- Signals that are read and not modified by this operator
-    reads = []
-    # -- Signals that are only assigned by this operator
-    sets = []
-    # -- Signals that are incremented by this operator
-    incs = []
-    # -- Signals that are updated to their [t + 1] value.
-    #    After this operator runs, these signals cannot be
-    #    used for reads until the next time step.
-    updates = []
+
+    # N.B. It is done on purpose that there are no default values for
+    # reads, sets, incs, and updates.
+    #
+    # Each operator should explicitly set each of these properties.
+
+    @property
+    def reads(self):
+        """Signals that are read and not modified"""
+        return self._reads
+
+    @reads.setter
+    def reads(self, val):
+        self._reads = val
+
+    @property
+    def sets(self):
+        """Signals assigned by this operator
+
+        A signal that is set here cannot be set or updated
+        by any other operator.
+        """
+        return self._sets
+    @sets.setter
+    def sets(self, val):
+        self._sets = val
+
+    @property
+    def incs(self):
+        """Signals incremented by this operator
+
+        Increments will be applied after this signal has been
+        set (if it is set), and before reads.
+        """
+        return self._incs
+
+    @incs.setter
+    def incs(self, val):
+        self._incs = val
+
+    @property
+    def updates(self):
+        """Signals assigned their value for time t + 1
+
+        This operator will be scheduled so that updates appear after
+        all sets, increments and reads of this signal.
+        """
+        return self._updates
+
+    @updates.setter
+    def updates(self, val):
+        self._updates = val
 
     @property
     def all_signals(self):
-        # -- Sanity check that no one has accidentally modified
-        #    these class variables, they should be empty
-        assert not Operator.reads
-        assert not Operator.sets
-        assert not Operator.incs
-        assert not Operator.updates
-
         return self.reads + self.sets + self.incs + self.updates
 
     def init_sigdict(self, sigdict, dt):
@@ -410,6 +445,9 @@ class Reset(Operator):
         self.dst = dst
         self.value = float(value)
 
+        self.reads = []
+        self.incs = []
+        self.updates = []
         self.sets = [dst]
 
     def __str__(self):
@@ -433,10 +471,12 @@ class Copy(Operator):
         self.dst = dst
         self.src = src
         self.tag = tag
+        self.as_update = True
 
         self.reads = [src]
         self.sets = [] if as_update else [dst]
         self.updates = [dst] if as_update else []
+        self.incs = []
 
     def __str__(self):
         return 'Copy(%s -> %s, as_update=%s)' % (
@@ -492,6 +532,8 @@ class DotInc(Operator):
 
         self.reads = [self.A, self.X]
         self.incs = [self.Y]
+        self.sets = []
+        self.updates = []
 
     def __str__(self):
         return 'DotInc(%s, %s -> %s "%s")' % (
@@ -525,6 +567,8 @@ class ProdUpdate(Operator):
 
         self.reads = [self.A, self.X, self.B]
         self.updates = [self.Y]
+        self.incs = []
+        self.sets = []
 
     def __str__(self):
         return 'ProdUpdate(%s, %s, %s, -> %s "%s")' % (
