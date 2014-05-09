38a9422c53ac966d2f251b5c1a76b37345cade4b
Trevor Bekolay <tbekolay@gmail.com>
2014-05-09 08:44:21 -0400

    [nengo->sigops] Removed population/encoder/decoder

--- nengo/simulator.py
+++ nengo/simulator.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from neuron import lif_step
-
 class Simulator(object):
     def __init__(self, model):
         self.model = model
@@ -10,7 +8,6 @@ class Simulator(object):
         self.signals = {}
         self.signals_tmp = {}
         self.signals_copy = {}
-        self.populations = {}
         self.probe_outputs = {}
 
         for sig in self.model.signals:
@@ -18,44 +15,10 @@ class Simulator(object):
             self.signals_tmp[sig] = np.zeros(sig.n)
             self.signals_copy[sig] = np.zeros(sig.n)
 
-        for pop in self.model.populations:
-            self.populations[pop] = {
-                'ic': np.zeros(pop.n),
-                'v': np.zeros(pop.n),
-                'rt': np.zeros(pop.n),
-                'out': np.zeros(pop.n),
-                'jbias': np.zeros(pop.n),
-            }
-
         for probe in self.model.signal_probes:
             self.probe_outputs[probe] = []
 
     def step(self):
-        for pop in self.model.populations:
-            self.populations[pop]['ic'] = 1.0 * self.populations[pop]['jbias']
-
-        for enc in self.model.encoders:
-            self.populations[enc.pop]['ic'] += np.dot(self.signals[enc.sig],
-                                                      enc.weights.T)
-
-        for pop in self.model.populations:
-            lif_step(
-                self.populations[pop]['ic'],
-                self.populations[pop]['v'],
-                self.populations[pop]['rt'],
-                self.populations[pop]['out'],
-                dt=self.model.dt,
-                #tau_rc=pop.tau_rc,
-                #tau_ref=pop.tau_ref,
-                tau_rc=0.02,
-                tau_ref=0.002,
-                upsample=1,
-                )
-
-        for dec in self.model.decoders:
-            self.signals_tmp[dec.sig] = np.dot(self.populations[dec.pop]['out'],
-                                               dec.weights.T)
-
         for sig in self.model.signals:
             self.signals_copy[sig] = self.signals[sig]
 
@@ -82,4 +45,3 @@ class Simulator(object):
 
     def signal_probe_output(self, probe):
         return self.probe_outputs[probe]
-
